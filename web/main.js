b4951f26bb25c7f37ad452deda0e9de21e214828
xyx <xanxys@gmail.com>
2019-09-16 17:08:40 +0900

    Cell random diffusion swapping

--- web/main.js
+++ web/main.js
@@ -1,134 +1,8 @@
 "use strict";
 import {Vec2, AABB} from "./math.js";
 
-let particles = [];
-
-const deg_split = 3;
-const deg_split2 = 3;
-
-const deg_kill_lower = 1;
-const deg_kill_upper = 6;
-
-/*
-I: stable
-II: repetitive
-III: chaotic
-IV: edge of chaos
-
--- SPL=REFLECT-a
-
-S=1,2 K=0,>=5
- 
-S=1,3 K=0,>=5
- 0.95
-
-Search good a.
-
-
--- SPL=REFLECT-HALF (K=0)
-S=1 K>=2,3,4,5: I
-S=1,2 K>=4: I+glider
-
-
--- SPL=REFLECT
-S=1 K>=2: I
-S=1 K>=3: I
-S=1 K>=4: I
-
-* S=2 K>=3: I, III, potentially IV, rare gliders
-S=2 K>=4: III, monotonic expansion of III region
-S=2 K>=5: III, gliders often exist, but destroyed by III region
-
-S=1,2 K>=3: III
-S=1,2 K>=4: III
-S=1,2 K>=5: III
-
-S=1,3 K>=4: multiscale III, too much garbage remaining
-S=1,3 K>=5: III
-S=1,3 K>=6: III, (IV), big glider destroyed by III region
-
-S=2,3 K>=4: III
-S=2,3 K>=5: III, linear glider destroyed by III region
-S=2,3 K>=6: III
-
--- SPL=REFLECT (K=0)
-S=1 K>=2: I, nothing
-S=1 K>=3: I, stable triangles
-S=1 K>=4: I, stable meshes
-S=1 K>=5: I, stable meshes
-
-S=2 K>=3: III -> I
-S=2 K>=4: III, monotonic expansion of chaotic region
-S=2 K>=5: III, monotonic expansion of chaotic region + quick-dying puffers
-
-S=1,2 K>=3: III -> I
-S=1,2 K>=4: III -> I
-* S=1,2 K>=5: often I, II, Potentially IV
-S=1,2 K>=6: III -> I, mesh remains
-
-S=1,3 K>=4: III -> I
-* S=1,3 K>=5: Potential IV, ladder-like puffer
-S=1,3 K>=6: III, micro+mesoscale III
-
-S=2,3 K>=4: III, (IV), ocassional ladder-glider destroyed by III region
-S=2,3 K>=5: III, (IV), ocassional ladder-glider destroyed by III region
-S=2,3 K>=6: III
-
--- SPL=REFLECT (K=1)
-S=2 K>=3: I
-S=2 K>=4: I, gliders
-S=2 K>=5: I, gliders
-S=2 K>=6: I, meshes
-
-S=3 K>=4: I
-S=3 K>=5: III, II -> I
-S=3 K>=6: III
-
-S=2,3 K>=4: IV->I, glider+puffer
-S=2,3 K>=5: II, III -> III
-S=2,3 K>=6: III
-
--- SPL=REFLECT (K=0,1)
-S=2 K>=3: I, glider exist, no replication
-S=2 K>=4: I
-S=2 K>=5: I
-
-S=3 K>=4: I
-S=3 K>=5: I
-S=3 K>=6: III
-
-S=2,3 K>=4: III+IV -> I, gliders exist
-S=2,3 K>=5: III (IV) -> I
-S=2,3 K>=6: III (IV)
-
-
--- SPL=KEEP + REFLECT (incl. K=0)
-S=1 K>=2: I, II
-S=1 K>=3: I, (II)
-S=1 K>=4: I, (II), linear growth that stops when colliding
-S=1 K>=5: I, linear growth that stops when colliding
-S=1 K>=6: I, linear growth that stops when colliding
-
-S=2 K>=3: (II), III, lattice-like
-S=2 K>=4: III
-S=2 K>=5: III -> I
-S=2 K>=6: I
-
-S=1,2 K>=3: II, (III)
-S=1,2 K>=4: III
-S=1,2 K>=5: II, III, noise between lattice blobs
-S=1,2 K>=6: I, almost stable polycrystal
-
--- SPL=KEEP + REFLECT-AVG
-S=2 K>=3: I
-S=2 K>=4: I
-S=2 K>=5: I, mesh coarsing
-
-S=2 K=0,K>=3: I
-S=2 K=0,K>=4: I
-S=2 K=0,K>=5: I, mesh coarsing
-
-*/
+const size = 128;
+const world = new Uint8Array(size * size);
 
 
 function redraw(scale, origin, box) {
@@ -149,159 +23,64 @@ function redraw(scale, origin, box) {
     ctx.translate(origin.x, origin.y);
     ctx.scale(scale, scale);
 
-    const num_particles = particles.length;
-    const degree = new Array(num_particles);
-    degree.fill(0);
-    ctx.beginPath();
-    for (var i = 0; i < num_particles; i++) {
-        for (var j = 0; j < i; j++) {
-            var p = particles[i];
-            var q = particles[j];
-            if (sqdist(p, q) <= 1) {
-                degree[i]++;
-                degree[j]++;
-
-                ctx.moveTo(p.x, p.y);
-                ctx.lineTo(q.x, q.y);
-            }
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const v = world[i + size * j];
+            ctx.fillStyle = `rgba(${v},${v},${v}, 1)`;
+            ctx.fillRect(i, j, 1, 1);
         }
     }
-    ctx.lineWidth = 0.03;
-    ctx.strokeStyle = "gray";
-    ctx.stroke();
-    
-    particles.forEach((p, ix) => {
-        const deg = degree[ix];
-        if (deg === deg_split || deg === deg_split2) {
-            ctx.fillStyle = "blue";
-        } else if (deg <= deg_kill_lower || deg >= deg_kill_upper) {
-            ctx.fillStyle = "red";
-        } else {
-            ctx.fillStyle = "black";
-        }
 
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, 0.1, 0, 2 * Math.PI);
-        ctx.fill();
-    });
     ctx.restore();
 
-    let rule_text = "";
-    if (deg_kill_lower < 0) {
-        rule_text += `d>=${deg_kill_upper}: die / `;
-    } else {
-        rule_text += `d<=${deg_kill_lower},>=${deg_kill_upper}: die / `;
-    }
-    
-    if (deg_split2 === null) {
-        rule_text += `d==${deg_split}: split`;
-    } else {
-        rule_text += `d==${deg_split},${deg_split2}: split`;
-    }
-    $("#label_rule").text(rule_text);
+    $("#label_rule").text("");
 }
 
 function cleanParticles() {
-    particles = [];
+    world.fill(0);
 }
 
 function addRandom(num) {
-    for (let i = 0; i < num; i++) {
-        particles.push(new Vec2(Math.random() * 20 + 15, Math.random() * 20 + 15));
+    for (let i = 0; i < size * size; i++) {
+        if (Math.random() < 0.05) {
+            world[i] = Math.floor(Math.random() * 255);
+        } else {
+            world[i] = 0;
+        }
+        
     }
 }
 
 function addPatternsRandomly(pat, num) {
-    const npat = normalizePos(pat);
-    for (let i = 0; i < num; i++) {
-        const dp = new Vec2(Math.random() * 25, Math.random() * 25);
-        const theta = Math.random() * (2 * Math.PI);
-
-        const c = Math.cos(theta);
-        const s = Math.sin(theta);
-        npat.forEach(p => particles.push(p.clone().rotate(c, s).add(dp)));
-    }
 }
-
-function sqdist(p, q) {
-    return (p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y);
-}
-
-function reflect(ref, p) {
-    const dx = p.x - ref.x;
-    const dy = p.y - ref.y;
-    return {x: ref.x - dx, y: ref.y - dy};
-}
-
-function normalizePos(ps) {
-    const res = new Vec2(0, 0);
-    ps.forEach(p => res.add(p));
-    const center = res.mult(1.0 / ps.length);
-    return ps.map(p => p.clone().sub(center));
-}
-
-function parse(pjson) {
-    return pjson.map(p => new Vec2(p.x, p.y));
-}
-
-const puffer1 = parse([{"x":-3.251099453334959,"y":-12.621436972933157},{"x":-3.5444067296287187,"y":-13.253306053867515},{"x":-3.8377140059224786,"y":-13.885175134801873},{"x":-3.5444067296287187,"y":-13.253306053867515},{"x":-4.424328558509998,"y":-15.148913296670589},{"x":-4.277674920363118,"y":-14.83297875620341},{"x":-4.131021282216238,"y":-14.517044215736231},{"x":-3.8377140059224786,"y":-13.885175134801873},{"x":-3.251099453334959,"y":-12.621436972933157}]);
-const puffer2 = parse([{"x":89.77556284296583,"y":-69.66371091975707},{"x":88.47423868126941,"y":-68.78149397842473},{"x":89.77556284296583,"y":-69.66371091975707},{"x":88.85044744413,"y":-68.11830202426692},{"x":91.74295559382608,"y":-67.7836475508709},{"x":91.74295559382608,"y":-67.7836475508709},{"x":90.78702717835898,"y":-72.64501621715009},{"x":90.78702717835898,"y":-72.64501621715009},{"x":90.23812054238374,"y":-70.43641536750214},{"x":91.07688700466224,"y":-70.5459278610894},{"x":91.07688700466224,"y":-70.5459278610894},{"x":92.55090903947331,"y":-74.29993760622749},{"x":93.47602443830914,"y":-75.84534650171763},{"x":93.47602443830914,"y":-75.84534650171763},{"x":95.3262552359808,"y":-78.93616429269791},{"x":96.54123046111988,"y":-78.38248483212736},{"x":96.91743922398047,"y":-77.71929287796955},{"x":95.61611506228405,"y":-76.83707593663722},{"x":97.09013709709512,"y":-80.59108568177531},{"x":97.69762470966467,"y":-80.31424595149004},{"x":97.92890355937362,"y":-80.70059817536257},{"x":97.8425546228163,"y":-79.26470177345969},{"x":97.0037881605378,"y":-79.15518927987243},{"x":96.62757939767721,"y":-79.81838123403024},{"x":97.46634585995571,"y":-79.9278937276175},{"x":97.0037881605378,"y":-79.15518927987243},{"x":95.3262552359808,"y":-78.93616429269791},{"x":95.78881293539871,"y":-79.70886874044298},{"x":96.54123046111988,"y":-78.38248483212736},{"x":96.91743922398047,"y":-77.71929287796955},{"x":97.75620568625897,"y":-77.82880537155681},{"x":98.21876338567688,"y":-78.60150981930188},{"x":97.20729905028372,"y":-75.62020452190886},{"x":97.29364798684105,"y":-77.05610092381174},{"x":97.20729905028372,"y":-75.62020452190886},{"x":96.36853258800522,"y":-75.5106920283216},{"x":93.47602443830914,"y":-75.84534650171763},{"x":93.09981567544855,"y":-76.50853845587544},{"x":93.09981567544855,"y":-76.50853845587544},{"x":95.35706825261207,"y":-72.52938673092858},{"x":95.4434171891694,"y":-73.96528313283146},{"x":95.4434171891694,"y":-73.96528313283146},{"x":94.51830179033357,"y":-72.41987423734132},{"x":92.55090903947331,"y":-74.29993760622749},{"x":91.62579364063748,"y":-72.75452871073735},{"x":92.37821116635865,"y":-71.42814480242173},{"x":94.51830179033357,"y":-72.41987423734132},{"x":93.50683745494041,"y":-69.4385689399483},{"x":93.50683745494041,"y":-69.4385689399483},{"x":92.66807099266191,"y":-69.32905644636104},{"x":92.66807099266191,"y":-69.32905644636104},{"x":92.66807099266191,"y":-69.32905644636104},{"x":88.85044744413,"y":-68.11830202426692}]);
-const glider1 = parse([{"x":66.29054711255714,"y":172.30115399605788},{"x":67.29100366212229,"y":172.46509825411027},{"x":67.6609864202613,"y":173.82518521242548},{"x":68.29146021168745,"y":172.62904251216267},{"x":68.73088739694686,"y":172.30623235491166},{"x":68.06871900550912,"y":173.56713560187288},{"x":67.06826245594397,"y":173.40319134382048},{"x":66.06780590637881,"y":173.23924708576808}]);
 
 function step() {
-    const num_particles = particles.length;
+    for (let microstep = 0; microstep < size * size; microstep++) {
+        const x = Math.floor(size * Math.random());
+        const y = Math.floor(size * Math.random());
 
-    const degree = new Array(num_particles);
-    degree.fill(0);
-    const degreeHalf = new Array(num_particles);
-    degreeHalf.fill(0);
-
-    for (let i = 0; i < num_particles; i++) {
-        for (let j = 0; j < i; j++) {
-            const p = particles[i];
-            const q = particles[j];
-            const d = sqdist(p, q);
-            if (d <= 0.25) {
-                degreeHalf[i]++;
-                degreeHalf[j]++;
-            } else if (d <= 1) {
-                degree[i]++;
-                degree[j]++;
-            }
-        }
-    }
-
-    const new_particles = [];
-    for (let i = 0; i < num_particles; i++) {
-        const p = particles[i];
-        const degh = degreeHalf[i];
-        const deg = degree[i];
-        const degf = degh + deg;
-
-        if (degh <= 0 || degf >= 6) {
-            // die
-        } else if (deg === 2 || deg === 3) {
-            // split
-            for (let j = 0; j < num_particles; j++) {
-                if (i == j) {
-                    continue;
-                }
-                const q = particles[j];
-                const d = sqdist(p, q);
-                if (0.5 < d && d <= 1) {
-                    new_particles.push(reflect(p, q));
-                }
-            }
+        let xp, yp;
+        if (Math.random() > 0.5) {
+            // horizontal swap
+            xp = (x + 1) % size;
+            yp = y;
         } else {
-            // keep
-            new_particles.push(p);
-        }        
+            // vertical swap
+            xp = x;
+            yp = (y + 1) % size;
+        }
+
+        const ix0 = x + size * y;
+        const ix1 = xp + size * yp;
+
+        const t = world[ix1];
+        world[ix1] = world[ix0];
+        world[ix0] = t;
     }
-    particles = new_particles;
 }
 
 function getParticlesIn(box) {
-    return particles.filter(p => box.contains(p));
+    return [];
 }
 
 function main() {
@@ -383,7 +162,7 @@ function main() {
                 this.redraw();
             },
             redraw: function() {
-                this.numParticles = particles.length;
+                this.numParticles = 0;
                 redraw(this.viewportScale, this.viewportOrigin, this.selectionBox);
             },
             zoom: function(ev) {
@@ -406,7 +185,6 @@ function main() {
                 }
                 this.interval = setInterval(() => {
                     step();
-                    this.numParticles = particles.length;
                     this.tick += 1;
                     this.redraw();
                 }, 50);    
@@ -420,7 +198,6 @@ function main() {
             },
             clickStep: function() {
                 step();
-                this.numParticles = particles.length;
                 this.tick += 1;
                 this.redraw();
             },
